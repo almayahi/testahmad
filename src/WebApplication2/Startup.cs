22bda531057de4f4b49bc25e9808fbbdba59c949
almayahi <ahm23@le.ac.uk>
2017-02-27 20:44:58 +0300

    pub2

--- src/WebApplication2/Startup.cs
+++ src/WebApplication2/Startup.cs
@@ -8,7 +8,7 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
 /// <summary>
-/// /////////////////////////////////////////////
+/// 
 /// 
 /// </summary>
 namespace WebApplication2
